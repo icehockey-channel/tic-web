509d49a2e6639ec0a0bf8281fe31c1926cf16e26
muun <nomjolno@gmail.com>
2019-09-01 01:16:25 +0900

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .container
+    h1 エラーが発生しました
+    nuxt-link(to='/') top page
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style  scoped>
+.container{
+    text-align: center;
+}
+</style>
